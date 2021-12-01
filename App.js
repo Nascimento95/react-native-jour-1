a229fd3d2d368915587dfa5cb898d5aa0954f5f0
Nascimento95 <marco-pereira@hotmail.fr>
2021-12-01 15:52:45 +0100

    decouverte de react native fini

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, ActivityIndicator, View, SafeAreaView, ScrollView, Image, Pressable, Button, Alert} from 'react-native';
 
 export default function App() {
+
+  const [Pressed, setPressed] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>sa marche pas !!!! hahahah </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      
+        <Text style={styles1.text} >sa marche pas !!!! hahahah </Text>
+        <Text style={styles1.text} >je suis le boss</Text>
+        <Text style={styles1.text} >de react native benoit !! </Text>
+        <Image
+        style={divImage.container}
+        source={{
+          uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',
+        }}
+        />
+        <Image
+          style={divImage.container}
+          source={{
+          uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',
+          }}
+        />
+        <Image
+        style={divImage.container}
+        source={require('./Asset/konexio-logo_1.png')}
+        />
+
+        <Button
+          title="Press me"
+          onPress={() => Alert.alert('vous avez appuyer ')}
+        />
+        <StatusBar style="auto" />
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="white" />
+        </View>
+        
+      </ScrollView>
+      
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  tinyLogo: {
+    width :70,
+    height: 70,
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+ 
+});
+const divImage =  StyleSheet.create({
+  container: {
+    height: 80,
+    justifyContent: 'center',
+    marginVertical: 50,
+  }
+})
+const styles1 = StyleSheet.create({
+  container: {
+    borderColor: 'white',
+    backgroundColor: 'white',
+    
+  },
+  text: {
+    fontSize: 30,
+    color:'red',
+    textAlign : "center",
+    fontWeight: "bold",
+    paddingVertical: 80,
   },
 });
