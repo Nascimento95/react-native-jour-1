e5d413183fef283fce025d30d1517e3767d6d363
Nascimento95 <marco-pereira@hotmail.fr>
2021-12-01 14:02:49 +0100

    dernier commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sa marche pas !!!! </Text>
+      <Text>sa marche pas !!!! hahahah </Text>
       <StatusBar style="auto" />
     </View>
   );
